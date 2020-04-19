0fccbeb58854d74ea39ac1bb58ff2e0e6f119658
Stoob <steejerbs@gmail.com>
2020-04-18 20:30:16 -0500

    Removed functionality handled by CFW in contraption.lua
    Removed GetAllChildren
    Removed GetAllPhysicalEntities

--- lua/acf/server/contraption.lua
+++ lua/acf/server/contraption.lua
@@ -1,40 +1,3 @@
-local function GetAllPhysicalEntities(Ent, Tab)
-	local Res = Tab or {}
-
-	if IsValid(Ent) and not Res[Ent] then
-		Res[Ent] = true
-
-		if Ent.Constraints then
-			for K, V in pairs(Ent.Constraints) do
-				if not IsValid(V) then -- Constraints don't clean up after themselves
-					Ent.Constraints[K] = nil -- But we will do Garry a favor and clean up after him
-					continue
-				end
-
-				if V.Type ~= "NoCollide" then -- NoCollides aren't a real constraint
-					GetAllPhysicalEntities(V.Ent1, Res)
-					GetAllPhysicalEntities(V.Ent2, Res)
-				end
-			end
-		end
-	end
-
-	return Res
-end
-
-local function GetAllChildren(Ent, Tab)
-	local Res = Tab or {}
-
-	for _, V in pairs(Ent:GetChildren()) do
-		if not IsValid(V) or Res[V] then continue end
-
-		Res[V] = true
-		GetAllChildren(V, Res)
-	end
-
-	return Res
-end
-
 do -- Contraption awareness/CFW -----------------
 	hook.Add("OnSetMass", "ACF", function(Ent, _, NewMass)
 		if Ent.ACF and Ent.ACF.LegalMass and NewMass ~= Ent.ACF.LegalMass then
@@ -46,7 +9,6 @@ do -- Contraption awareness/CFW -----------------
 	hook.Add("OnContraptionAppend", "ACF ColGroups", function(_, Ent)
 		if ColGroupFilter[Ent:GetCollisionGroup()] then -- If the collision group is set to something we dont like
 			Ent:SetCollisionGroup(COLLISION_GROUP_NONE) -- Reset it
-
 		end
 	end)
 end
@@ -81,6 +43,3 @@ do -- ACF Parent Detouring ----------------------
 		hook.Remove("Initialize", "ACF Parent Detour")
 	end)
 end ---------------------------------------------
-
--- Globalize ------------------------------------
-ACF.GetAllChildren 			= GetAllChildren
